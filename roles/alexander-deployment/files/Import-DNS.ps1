42dbd568d71b13904aa55d52ff9b4a80feece4c9
Joe Graham <jmg3484@rit.edu>
2019-02-21 23:27:05 -0500

    implement dns for alex

---
+++ roles/alexander-deployment/files/Import-DNS.ps1
@@ -0,0 +1,12 @@
+# Import A records from CSV file.
+
+param (
+     [Parameter(Mandatory=$true)][string]$Filename,
+     [Parameter(Mandatory=$true)][string]$TeamNumber
+)
+
+$csv = Import-Csv $Filename
+foreach ($record in $csv){
+    $ZoneName = "team" + $TeamNumber + ".ists.io"
+     Add-DnsServerResourceRecordA -Name $record.Name -IPv4Address $record.Address -ZoneName $ZoneName
+}
